c1ace10fe6346d93579a7829296d811f5addc790
alejmonts <alejmonts97@gmail.com>
2020-07-15 13:49:51 -0500

    checar el nav 1:20

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let btnMenu = document.querySelector('.btn-menu');
 let menu = document.querySelector('.list-container');
-let menu = document.querySelector('.menu');
+let containerMenu = document.querySelector('.menu');
 let activador = true;
 
 btnMenu.addEventListener('click', () => {
+
+    document.querySelector('.btn-mennu i').classList.toggle('fa-times');
+
     if(activador){
         
         menu.style.left = "0";
@@ -16,6 +19,21 @@ btnMenu.addEventListener('click', () => {
     menu.style.left = "-100%";
     menu.style.transition = "0.5s";
 
-    activadro = true;
+    activador = true;
     }
 });
+
+let enlaces = document.querySelectorAll('.lists li a');
+
+    enlaces.forEach( (element) => {
+
+        element.addEventListener('click', (event) => {
+
+            enlaces.forEach( (link) => {
+                link.classList.remove('activo');
+                
+            });
+
+        });
+        
+    });
