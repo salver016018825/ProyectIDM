7b4053b2aed938f65bfc64101670ba24dd498fc1
salver <016018825@upq.edu.mx>
2020-09-14 22:14:11 -0500

    Se agrega galeria estatica a moldes

--- js/app.js
+++ js/app.js
@@ -108,20 +108,3 @@ addEventListener('scroll', () => {
 })
 
 
-/*
-  animacion moldes
-*/
-let animado = document.querySelectorAll(".animado");
-
-function mostrarScroll(){
-    let scrollTop = document.documentElement.scrollTop;
-    for(var i = 0; i < animado.length; i++){
-        let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado - 30 < scrollTop){
-            animado[i].style.opacity = 1;
-            animado[i].classList.add("mostrarArriba");
-        }
-    }
-}
-
-window.addEventListener('scroll',mostrarScroll);
