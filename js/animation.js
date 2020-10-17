1284ad1983e8dab029115cabafdd486b05dda3d2
salver <016018825@upq.edu.mx>
2020-10-17 18:41:10 -0500

    se realiza responsi movil mejorar para otros tamanios

--- js/animation.js
+++ js/animation.js
@@ -4,7 +4,7 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i = 0; i < animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado - 50 < scrollTop){
+        if(alturaAnimado - 1 < scrollTop){
             animado[i].style.opacity = 1;
             animado[i].classList.add("mostrarArriba");
             animado[i].classList.add('mostrar-texto');
