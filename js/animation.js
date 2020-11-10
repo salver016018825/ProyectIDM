25262df9ae51c57b289f181e39afc4c902c57c43
salver <016018825@upq.edu.mx>
2020-11-09 19:22:14 -0600

    Se agrega particules a automatizacion y se congifura responsive

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ let animado = document.querySelectorAll(".animado");
 
 function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
-    for(var i = 0; i < animado.length; i++){
+    for(var i = 0; i <= animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
         if(alturaAnimado - 1 < scrollTop){
             animado[i].style.opacity = 1;
