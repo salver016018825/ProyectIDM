da5814a79989683f36c4a7c17069dc94db68f979
salver <016018825@upq.edu.mx>
2020-11-14 00:17:46 -0600

    Se agrega configuracion del correo envia.php

---
+++ conf/envia.php
@@ -0,0 +1,16 @@
+﻿<?php
+    $destinatario = 'salver.osorio@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    
+
+    $header = "Mensaje Enviado";
+    $mensajeCompleto = $mensaje."\nAtentamente. $nombre." \n".$correo;
+    mail($destinatario,$asunto,$mensajeCompleto,$header);
+    echo"<script>alert('correo enviado exitosamente')</script>";
+    echo"<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
