dd1f4a91ea2618da67f4ce9aa8225c777c2346c8
Salver <016018825@upq.edu.mx>
2020-12-02 13:07:32 -0600

    Se modifica texto en valores y en futuros proyectos, acomoda responsive la parte index -> contacto pdf

--- conf/envia.php
+++ conf/envia.php
@@ -5,9 +5,13 @@
     $asunto = $_POST['asunto'];
     $mensaje = $_POST['mensaje'];
     $header = "Mensaje Enviado";
-    $mensajeCompleto = $mensaje."\nAtentamente.$nombre." \n".$correo;
-    mail($destinatario,$asunto,$mensajeCompleto,$header);
-    echo"<script>alert('correo enviado exitosamente')</script>";
-    echo"<script> setTimeout(\"location.href='../web/contacto1.html'\",1000)</script>";
+    $mensajeCompleto = $mensaje."\nAtentamente".$nombre." \n".$correo;
+    if(mail($destinatario,$asunto,$mensajeCompleto)){
+        echo"<script>alert('correo enviado exitosamente')</script>";
+        echo"<script> setTimeout(\"location.href='../web/contacto1.html'\",1000)</script>";
+    }else{
+        echo"<script>alert('error al enviar correo intenete nuevamewnte')</scrpt>";
+    }
 
 ?>
+
