d1069270482c08c837649bae04c485a857b71bc0
Salver <016018825@upq.edu.mx>
2020-12-18 22:44:21 -0600

    Se finaliza contacto con php y html

--- conf/envia.php
+++ conf/envia.php
@@ -1,17 +1,19 @@
-﻿<?php
-    $destinatario = 'salver.osorio@gmail.com';
-    $nombre = $_POST['nombre'];
-    $correo = $_POST['correo'];
-    $asunto = $_POST['asunto'];
-    $mensaje = $_POST['mensaje'];
+<?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );   
+    $destinatario = 'idmetalmecanico@aol.com'; //contacto@idm-industrial.com
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
     $header = "Mensaje Enviado";
-    $mensajeCompleto = $mensaje."\nAtentamente".$nombre." \n".$correo;
+    $mensajeCompleto = "Hola IDM Industrial haz recibido un nuevo correo. "."\n\n\nDe: ".$nombre."\n"."E-Mail: ".$correo." \n"."\n\n".$mensaje;
     if(mail($destinatario,$asunto,$mensajeCompleto)){
         echo"<script>alert('correo enviado exitosamente')</script>";
-        echo"<script> setTimeout(\"location.href='index.html'\",1000)</script>";
-    }else{
-        echo"<script>alert('error al enviar correo intenete nuevamente')</scrpt>";
+        echo"<script> setTimeout(\"location.href='../index.html'\",1000)</script>";
     }
 
 ?>
+
+
 
