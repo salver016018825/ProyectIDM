cb7112375050e8961cbb35ec6d59afb95e920a04
Salver <016018825@upq.edu.mx>
2021-04-15 14:34:03 -0500

    borrado de archivos
    Se depura archivos del proyecto

--- js/moldes.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const imgLightBox = document.querySelectorAll('.materialboxed');
-	M.Materialbox.init(imgLightBox, {
-		inDuration: 500,
-		outDuration: 500
-	});
-});
